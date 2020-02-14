0396919ba61888216d2621c263a40899b461d77d
GreyProgramming <morgangreyprofessional@gmail.com>
2020-02-14 16:24:36 +0000

    End of week4

---
+++ tests/test_back_end.py
@@ -0,0 +1,36 @@
+import unittest
+
+from flask import abort, url_for
+from flask_testing import TestCase
+
+from application import app, db
+from application.models import Users, Posts
+
+class TestBase(TestCase):
+	def create_app(self):
+		config_name = 'testing'				#pass in config for test db
+		app.config.update(
+			SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1Che4Mis0Try1!@35.189.121.243/flaskapp')
+		return app
+
+	def setUp(self):					#Called before every test
+		db.session.commit()				#empties test database at test start
+		db.drop_all()
+		db.create_all()
+
+								#Creating admin and non-admin tests
+		admin = Users(first_name="admin", last_name="admin", email="admin@user.com", password="admin2016")
+		employee = Users(first_name="test", last_name="user", email="test@user.com", password="test2016")
+
+		db.session.add(admin)				#Save users to database
+		db.session.add(employee)
+		db.session.commit()
+
+	def tearDown(self):					#Called after every test
+		db.session.remove()
+		db.drop_all()
+
+class TestViews(TestBase):					#Test that the homepage is available without login
+	def test_homepage_view(self):
+		response = self.client.get(url_for('home'))
+		self.assertEqual(response.status_code, 200)
